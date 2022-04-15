bfee793eb9e0d4d5847e47ba6177a05fb90ef962
Sahitya Vemparala <sahitya.v1102@gmail.com>
2022-04-15 21:34:12 +0530

    Connecting Components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
 <header><h1>My Friends list</h1></header>
+<new-friend @submit="submitDetails"></new-friend>
 <ul>
 <friend-contact
 v-for="friend in friends"
@@ -13,7 +14,10 @@ v-for="friend in friends"
 
 </ul>
 </template>
+
 <script>
+
+
 export default{
     data(){
         return{
@@ -33,7 +37,19 @@ export default{
         }
         ]
         }
-    }
+    },
+    methods: {
+      submitDetails(name,email,phone){
+       const NewFriendContact={
+         id:new Date().toISOString(),
+         name:name,
+         email:email,
+         phone:phone,
+         isFavourite:false
+       }
+this.friends.push(NewFriendContact)
+      }
+    },
 }
 
 </script>
@@ -68,7 +84,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -101,5 +118,17 @@ header {
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
 }
-
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
+}
 </style>
---
+++ src/components/NewFriend.vue
@@ -0,0 +1,44 @@
+<template>
+<form @submit.prevent="submitData">
+<h2>New Friend</h2>
+
+<div>
+<label> Name</label>
+<input type="text" v-model="ename">
+<div>
+<label> Email</label>
+<input type="text" v-model="eemail">
+</div>
+<div>
+<label> Phone</label>
+<input type="text" v-model="ephone">
+</div>
+<button >Submit</button>
+</div>
+</form>
+</template>
+<script>
+export default{
+    emits:[
+        'submit'
+    ],
+    data(){
+        return{
+            ename:'',
+            ephone:'',
+            eemail:''
+        }
+
+    },
+    methods:{
+        submitData(){
+            this.$emit('submit',
+            this.ename,
+            this.ephone,
+            this.eemail
+            )
+        }
+    }
+}
+</script>
+
