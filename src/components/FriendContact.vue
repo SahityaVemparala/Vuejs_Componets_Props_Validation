674ac83fd892bed1ce485bed1fc5e630f2c24a04
Sahitya Vemparala <sahitya.v1102@gmail.com>
2022-04-15 18:58:31 +0530

    first commit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,53 @@
+<template>
+    <li>
+    <h2 v-if="isFavourite">{{name}}{{isFavourite==='1'?'(Favourite)':''}}</h2>
+    <button @click="toggleDisplayData">{{detailsAreVisible?'Hide':'Show'}} Details</button>
+    <ul v-if="detailsAreVisible">
+    <li><strong>Phone:</strong>{{phoneNumber}}</li>
+    <li><strong>Email:</strong>{{emailAddress}}</li>
+    </ul>
+    </li>
+</template>
+<script>
+export default{
+    props:{
+        name:{
+            type:String,
+            required:true
+        },
+        phoneNumber:{
+            type:String,
+            required:true
+        },
+        emailAddress:{
+            type:String,
+            required:true
+        },
+        isFavourite:{
+            type:String,
+            required:false,
+            default:'0',
+            validator:function(value){
+                return value===1||value===0
+            }
+        }
+    },
+    data(){
+        return{
+            detailsAreVisible:false,
+            friend:{
+            id:1,
+            name:'Sahitya',
+            email:'sahitya.v@gmail.com',
+            phone:'012345'
+            }
+        }
+    },
+    methods:{
+        toggleDisplayData(){
+            this.detailsAreVisible=!this.detailsAreVisible;
+        }
+    }
+
+}
+</script>
